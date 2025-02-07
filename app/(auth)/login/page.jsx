a01558d69c00ef30c42b665c0695e891fd011034
Solomon Gilala <gilogilala@gmail.com>
2025-02-07 19:04:21 +0100

    login

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -9,15 +9,14 @@ import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
+  CardFooter,
 } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Checkbox } from "@/components/ui/checkbox";
 import toast from "react-hot-toast";
-import myAxios from "@/lib/axiosConfig";
 import { useRouter } from "next/navigation";
 import { signIn } from "next-auth/react";
 
@@ -25,15 +24,11 @@ const Login = () => {
   const [formData, setFormData] = useState({
     email: "john1@gmail.com",
     password: "12345678",
-    rememberMe: "",
+    rememberMe: false,
   });
   const [loading, setLoading] = useState(false);
 
   const router = useRouter();
-
-  // password: "12345678",
-  // email: "john1@gmail.com",
-  // phone: "09030904384",
 
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
@@ -42,22 +37,6 @@ const Login = () => {
       [name]: type === "checkbox" ? checked : value,
     }));
   };
-
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   try {
-  //     const res = await myAxios.post("/authentication/login", formData);
-  //     console.log("res.data :", res.data);
-  //     if (res.data.success) {
-  //       toast.success(res.data.message);
-  //       // Optionally, you can reset the form here:
-  //       router.push("/clients/sms");
-  //     }
-  //   } catch (error) {
-  //     console.error("Unexpected error:", error);
-  //     toast.error("An error occurred while creating the user.");
-  //   }
-  // };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -69,36 +48,23 @@ const Login = () => {
         email: formData.email,
         password: formData.password,
       });
-      console.log(res);
 
-      if (res.ok) {
-        setLoading(false);
+      if (res?.error) {
+        toast.error("Invalid email or password.");
+      } else if (res?.ok) {
+        toast.success("Login successful!");
         router.push("/dashboard");
       }
     } catch (error) {
       console.error("Unexpected error:", error);
       toast.error("An error occurred while logging in.");
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
     <>
-      {/* <div className="md:hidden">
-        <Image
-          src="/Alfridget-Logo.jpeg"
-          width={1280}
-          height={843}
-          alt="Authentication"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/Alfridget-Logo.jpeg"
-          width={1280}
-          height={843}
-          alt="Authentication"
-          className="hidden dark:block"
-        />
-      </div> */}
       <div className="container relative hidden h-svh flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <Link
           href="/signup"
@@ -112,14 +78,14 @@ const Login = () => {
         <div
           className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex"
           style={{
-            backgroundImage: `url('/bgLogin.png')`,
-            backgroundSize: "100% 100%",
-            backgroundRepeat: "no-repeat",
+            backgroundImage: `url('/regel.jpeg')`,
+            backgroundSize: "cover",
             backgroundPosition: "center",
           }}
         >
           <div className="absolute inset-0 bg-zinc-900/30" />
-          <div className="relative z-20 flex items-center text-2xl font-medium">
+          <Link  href="/"
+           className="relative z-20 flex items-center text-2xl font-medium">
             <Image
               src="/regel.jpeg"
               alt="Regel company logo"
@@ -127,8 +93,9 @@ const Login = () => {
               height={100}
               className="mr-2 h-10 w-10"
             />
-            Regel
-          </div>
+            Regel Technology
+          </Link>
+  
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
               <q className="text-lg font-medium italic">
@@ -142,19 +109,11 @@ const Login = () => {
         </div>
         <div className="lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]">
-            {/* <div className="flex flex-col space-y-2 text-center">
-              <h1 className="text-2xl font-semibold tracking-tight">
-                Create an account
-              </h1>
-              <p className="text-sm text-muted-foreground">
-                Enter your email below to create your account
-              </p>
-            </div> */}
             <Card>
               <CardHeader className="space-y-1">
-                <CardTitle className="text-2xl">Create an account</CardTitle>
+                <CardTitle className="text-2xl">Login</CardTitle>
                 <CardDescription>
-                  Enter your email below to create your account
+                  Enter your email and password to log in.
                 </CardDescription>
               </CardHeader>
               <CardContent className="grid gap-4">
@@ -185,57 +144,35 @@ const Login = () => {
                       id="rememberMe"
                       checked={formData.rememberMe}
                       onCheckedChange={(checked) =>
-                        setFormData({ ...formData, terms: checked })
+                        setFormData({ ...formData, rememberMe: checked })
                       }
+                      aria-label="Remember me"
                     />
                     <Label htmlFor="rememberMe">Remember Me</Label>
                   </div>
-                  <p className=" text-center text-sm ">
-                    <Link
-                      href="/terms"
-                      className="underline underline-offset-4 hover:text-primary"
-                    >
-                      Forgot Password
-                    </Link>
-                    .
-                  </p>
+                  <Link
+                    href="/resetPassword"
+                    className="text-sm text-blue-600 hover:underline"
+                  >
+                    Forgot Password?
+                  </Link>
                 </div>
               </CardContent>
-              <Link href="/clients">
-                <CardFooter>
-                  <Button onClick={handleSubmit} className="w-full">
-                    Login
-                  </Button>
-                </CardFooter>
-              </Link>
-
+              <CardFooter>
+                <Button
+                  onClick={handleSubmit}
+                  className="w-full"
+                  disabled={loading}
+                >
+                  {loading ? (
+                    <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                  ) : null}
+                  Login
+                </Button>
+              </CardFooter>
               <CardFooter className="flex-col">
-                {/* <div className="space-y-4 pb-3">
-                  <div className="relative">
-                    <div className="absolute inset-0 flex items-center">
-                      <span className="w-full border-t" />
-                    </div>
-                    <div className="relative flex justify-center text-xs uppercase">
-                      <span className="bg-background px-2 text-muted-foreground">
-                        Or continue with
-                      </span>
-                    </div>
-                  </div>
-
-                  <div className="grid grid-cols-2 gap-6">
-                    <Button variant="outline">
-                      <Icons.gitHub className="mr-2 h-4 w-4" />
-                      Github
-                    </Button>
-                    <Button variant="outline">
-                      <Icons.google className="mr-2 h-4 w-4" />
-                      Google
-                    </Button>
-                  </div>
-                </div> */}
-
                 <p className="px-8 text-center text-sm text-muted-foreground">
-                  By clicking continue, you agree to our
+                  By clicking continue, you agree to our{" "}
                   <Link
                     href="/terms"
                     className="underline underline-offset-4 hover:text-primary"
@@ -252,12 +189,12 @@ const Login = () => {
                   .
                 </p>
                 <p className="text-center text-sm mt-4">
-                  Already a Member?{" "}
+                  Don&apos;t have an account?{" "}
                   <Link
                     href="/signup"
                     className="text-blue-600 hover:underline"
                   >
-                    SignUp
+                    Sign Up
                   </Link>
                 </p>
               </CardFooter>
